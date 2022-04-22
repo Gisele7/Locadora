1db1458d3e85bd53087d057b2449231687e5ac83
Gisele da Silva Cidral <63521270+Gisele7@users.noreply.github.com>
2022-04-21 22:22:01 -0300

    Inicio da tela de login

---
+++ Locadora/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Locadora.Controllers
+{
+    public class LoginController : Controller
+    {
+        LocadoraContext locadoraContext;
+
+        public void Setup()
+        {
+            locadoraContext = new LocadoraContext();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+
+        [HttpPost]
+        public IActionResult Logar()
+        {
+            try
+            {
+                Logins ologin = new Logins();
+                locadoraContext.Logins.Add(ologin);
+                locadoraContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("erro ao logar.");
+
+            }
+            return Json(new { }); //colocando o retorno com Json, conseguimos fazer personalização nas msgs ou bootstrap tb, usando o DataAnnotation..
+
+        }
+        //[HttpPost]
+        //public void Login()
+        //{
+        //    try
+        //    {
+        //        Logins ologin = new Logins();
+        //        locadoraContext.Logins.Add(ologin);
+        //        locadoraContext.SaveChanges();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("Erro ao logar.");
+
+        //    }
+        //}
+    }
+}
